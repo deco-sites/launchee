62dc6aa812349377bcab0de3e5baf73b36e7ed91
Jeferson Vale <jefersonnpn@gmail.com>
2024-07-13 17:52:39 -0300

    :hammer: fix benefit listing

--- sections/Pricing.tsx
+++ sections/Pricing.tsx
@@ -56,7 +56,7 @@ export default function Plans({ plans = [] }: Pricing) {
                         <h2 class="text-2xl font-semibold mb-4">{ plan.planType }</h2>
                         <p class="text-3xl font-bold mb-4">{plan.price}</p>
                         {plan.benefits?.map(benefit => {
-                            <p class="text-gray-600 mb-4">{benefit.description}
+                            return <p class="text-gray-600 mb-4">{benefit.description}
                                 <span class="badge badge-accent">{benefit?.tag}</span></p>
                         })}
                         <button class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md">Choose Plan</button>
